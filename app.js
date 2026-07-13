27328b554298269edb449413891c100584cae91f
Aris Tzimkas <aristeidhstz@gmail.com>
2026-07-13 12:18:11 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+function showBar() {
+    const sidebar = document.querySelector('.mobile_navbar');
+    sidebar.style.display="flex";
+}
+function hideBar() {
+    const sidebar = document.querySelector('.mobile_navbar');
+    sidebar.style.display="none";
+}
